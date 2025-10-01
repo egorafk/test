7c19bd40aefab0eba1ed77828b38346dad7e4de2
egor <rrr12545@mail.ru>
2025-10-01 14:10:36 +0000

    ss

--- 1.sh
+++ 1.sh
@@ -3,19 +3,20 @@
 
 LOG="/var/log/monitoring.log"
 PID_FILE="/tmp/test.pid"
+URL="https://test.com/monitoring/test/api"
 
 PID=$(pgrep -x "test")
 
-if [ "PID" ]; then
-	if [ -f "PID_FILE" ]; then
+if [ -n "$PID" ]; then
+	if [ -f "$PID_FILE" ]; then
 		OLD=$(cat "$PID_FILE")
 		if [ "$OLD" != "$PID" ]; then
-			echo "Service was restarted" >> "$LOG"
+			echo "Service was restarted (old pid: $OLD, new pid: $PID)" >> "$LOG"
 		fi
 	fi
 	echo "$PID" > "$PID_FILE"
 
-	curl -s -f -L -o /dev/null --connect-timeout 10 "https://test.com/monitoring/test/api"
+	curl -s -f -L -o /dev/null --connect-timeout 10 "$URL"
 	if [ $? -ne 0 ]; then
 		echo "Monitoring server unavalaible" >> "$LOG"
 	fi
